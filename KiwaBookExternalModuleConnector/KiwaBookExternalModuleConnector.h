ef003248c99faa2dbb40fc888f7cf1f285665c79
Oleg Cherynshenko <oleg@kiwadigital.com>
2015-12-17 12:00:52 +1300

    Initial Commit

---
+++ KiwaBookExternalModuleConnector/KiwaBookExternalModuleConnector.h
@@ -0,0 +1,13 @@
+//
+//  KiwaBookExternalModuleConnector.h
+//  KiwaBookExternalModuleConnector
+//
+//  Created by Oleg Chernyshenko on 17/12/15.
+//  Copyright © 2015 Kiwa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KiwaBookExternalModuleConnector : NSObject
+
+@end
---
+++ KiwaBookExternalModuleConnector/KiwaBookExternalModuleConnector.m
@@ -0,0 +1,13 @@
+//
+//  KiwaBookExternalModuleConnector.m
+//  KiwaBookExternalModuleConnector
+//
+//  Created by Oleg Chernyshenko on 17/12/15.
+//  Copyright © 2015 Kiwa. All rights reserved.
+//
+
+#import "KiwaBookExternalModuleConnector.h"
+
+@implementation KiwaBookExternalModuleConnector
+
+@end
