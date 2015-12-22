355cc44aba54b1377bd58911dfc925762b5ebea4
Oleg Cherynshenko <oleg@kiwadigital.com>
2015-12-22 15:18:02 +1300

    updated embedding method

--- KiwaBookExternalModuleConnector/KiwaBookExternalModuleConnector.h
+++ KiwaBookExternalModuleConnector/KiwaBookExternalModuleConnector.h
@@ -6,8 +6,32 @@
 //  Copyright © 2015 Kiwa. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+//#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+extern NSString *kStartEmbeddedGameNotification;
+extern NSString *kCloseEmbeddedGameNotification;
+
+@protocol KiwaBookEmbeddingDelegate <NSObject>
+
+//- (void)closeGame;
+
+@end
+//
+@protocol KiwaBookEmbedding <NSObject>
+//- (void)startGame;
+//- (void)stopGame;
+@property (nonatomic, weak) id <KiwaBookEmbeddingDelegate> delegate;
+@property (nonatomic, strong) UIViewController *controller;
+
+@end
+
 
 @interface KiwaBookExternalModuleConnector : NSObject
+
+//+ (void)openGame:(int)gameIndex;
++ (id <KiwaBookEmbedding>)moduleWithNamePrefix:(NSString *)name;
+//@property (nonatomic, weak) id <KiwaBookEmbeddingDelegate> delegate;
+//@property (nonatomic, strong) UIViewController *controller;
 
 @end
--- KiwaBookExternalModuleConnector/KiwaBookExternalModuleConnector.m
+++ KiwaBookExternalModuleConnector/KiwaBookExternalModuleConnector.m
@@ -8,6 +8,17 @@
 
 #import "KiwaBookExternalModuleConnector.h"
 
+NSString *kStartEmbeddedGameNotification = @"kStartEmbeddedGameNotification";
+NSString *kCloseEmbeddedGameNotification = @"kCloseEmbeddedGameNotification";
+
 @implementation KiwaBookExternalModuleConnector
+
++ (id <KiwaBookEmbedding>)moduleWithNamePrefix:(NSString *)name {
+    NSString *controllerName = [NSString stringWithFormat:@"%@EmbeddingController", name];
+    id<KiwaBookEmbedding> object  = [[NSClassFromString(controllerName) alloc] init];
+    
+    return object;
+}
+
 
 @end
